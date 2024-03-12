a1f78d4a23f08de555928680638f2c35d6841c7b
yashashri w-27 <yashashriw27@gmail.com>
2024-03-12 13:13:31 +0530

    see if problem is solved

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Footer1 from './components/Footer1';
+import Navcontents from './components/Navcontents';
+function App() {
+  return (
+    <div className="App">
+    <Navcontents/>
+      <Footer1 />
+    </div>
+  );
+}
+
+export default App;
